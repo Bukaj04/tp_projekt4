8d21f26d2f151688b51a37da086d85de1fefe89e
oneesh00t <148895460+oneesh00t@users.noreply.github.com>
2024-06-14 20:27:37 +0200

    Update simulate.cpp

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -2,6 +2,7 @@
  * SDL window creation adapted from https://github.com/isJuhn/DoublePendulum
 */
 #include "simulate.h"
+using namespace matplot;
 
 Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     /* Calculate LQR gain matrix */
@@ -15,9 +16,9 @@ Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     Eigen::MatrixXf K = Eigen::MatrixXf::Zero(6, 6);
     Eigen::Vector2f input = quadrotor.GravityCompInput();
 
-    Q.diagonal() << 10, 10, 10, 1, 10, 0.25 / 2 / M_PI;
-    R.row(0) << 0.1, 0.05;
-    R.row(1) << 0.05, 0.1;
+    Q.diagonal() << 0.005, 0.005, 400, 0.01, 0.05, 0.25 / 2 / M_PI;
+    R.row(0) << 30, 10;
+    R.row(1) << 10, 30;
 
     std::tie(A, B) = quadrotor.Linearize();
     A_discrete = Eye + dt * A;
@@ -33,6 +34,7 @@ void control(PlanarQuadrotor &quadrotor, const Eigen::MatrixXf &K) {
 
 int main(int argc, char* args[])
 {
+    time_t start = time(0);
     std::shared_ptr<SDL_Window> gWindow = nullptr;
     std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
     const int SCREEN_WIDTH = 1280;
@@ -45,6 +47,8 @@ int main(int argc, char* args[])
      * 2. Update PlanarQuadrotor from simulation when goal is changed
     */
     Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
+    //initial_state << SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 0, 0, 0;
+
     PlanarQuadrotor quadrotor(initial_state);
     PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
     /**
@@ -56,7 +60,7 @@ int main(int argc, char* args[])
     goal_state << 0, 0, 0, 0, 0, 0;
     quadrotor.SetGoal(goal_state);
     /* Timestep for the simulation */
-    const float dt = 0.001;
+    const float dt = 0.2;
     Eigen::MatrixXf K = LQR(quadrotor, dt);
     Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
 
@@ -75,23 +79,64 @@ int main(int argc, char* args[])
         bool quit = false;
         float delay;
         int x, y;
+        float x_plot, y_plot, theta_plot;
         Eigen::VectorXf state = Eigen::VectorXf::Zero(6);
-
+        Eigen::Vector2f new_goal = Eigen::Vector2f::Zero(2);
+        int n = 1;
         while (!quit)
         {
             //events
             while (SDL_PollEvent(&e) != 0)
             {
+                Eigen::VectorXf z_plot = quadrotor.GetState();
+                x_plot = z_plot[0];
+                y_plot = z_plot[1];
+                theta_plot = z_plot[2];
+                double plot_insertion_timer = difftime(time(0), start);
+                if (plot_insertion_timer >= n) {
+                    x_history.push_back(x_plot);
+                    y_history.push_back(y_plot);
+                    theta_history.push_back(theta_plot);
+                    n++;
+                }
                 if (e.type == SDL_QUIT)
                 {
                     quit = true;
                 }
-                else if (e.type == SDL_MOUSEMOTION)
+                else if (e.type == SDL_MOUSEBUTTONDOWN)
                 {
                     SDL_GetMouseState(&x, &y);
-                    std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
+                    float quad_x = static_cast<float>(x) - SCREEN_WIDTH / 2;
+                    float quad_y = SCREEN_HEIGHT / 2 - static_cast<float>(y);
+                    goal_state << quad_x, quad_y, 0, 0, 0, 0;
+                    quadrotor.SetGoal(goal_state);
                 }
-                
+                else if (e.type == SDL_KEYUP)
+                {
+                    if (e.key.keysym.sym == SDLK_p) {
+                        double seconds_since_start = difftime(time(0), start); \
+                            std::vector<double> time_history = linspace(0, seconds_since_start);
+                        std::cout << time_history.size() << std::endl;
+
+                        tiledlayout(3, 1);
+                        auto ax1 = nexttile();
+                        auto p1 = plot(ax1, time_history, x_history, "-o");
+                        xlabel(ax1, "time");
+                        ylabel(ax1, "x");
+
+                        auto ax2 = nexttile();
+                        auto p2 = plot(ax2, time_history, y_history, "--xr");
+                        xlabel(ax2, "time");
+                        ylabel(ax2, "y");
+
+                        auto ax3 = nexttile();
+                        auto p3 = plot(ax3, time_history, theta_history, "-:gr");
+                        xlabel(ax3, "time");
+                        ylabel(ax3, "theta");
+
+                        show();
+                    }
+                }
             }
 
             SDL_Delay((int) dt * 1000);
